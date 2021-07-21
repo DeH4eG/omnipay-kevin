c16e5f32989f7f875558423b0c1a356e101bec04
Denis Smolakov <denissmolakovs@gmail.com>
2021-07-21 19:39:33 +0300

    FEAT: Add check if payment group is completed

--- src/Message/Response/FetchTransactionResponse.php
+++ src/Message/Response/FetchTransactionResponse.php
@@ -16,6 +16,11 @@ class FetchTransactionResponse extends AbstractResponse
     private const PAYMENT_RECEIVED = 'RCVD';
 
     /**
+     * @var string
+     */
+    private const PAYMENT_STATUS_GROUP_COMPLETED = 'completed';
+
+    /**
      * @inheritDoc
      */
     public function isSuccessful(): bool
@@ -32,10 +37,26 @@ class FetchTransactionResponse extends AbstractResponse
     }
 
     /**
+     * @return string|null
+     */
+    public function getPaymentGroup(): ?string
+    {
+        return $this->getValueFromData('group');
+    }
+
+    /**
      * @return bool
      */
     public function isPaymentReceived(): bool
     {
         return $this->getPaymentStatus() === self::PAYMENT_RECEIVED;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPaymentCompleted(): bool
+    {
+        return $this->getPaymentGroup() === self::PAYMENT_STATUS_GROUP_COMPLETED;
     }
 }
